bcdf970571e022ab07c1f2984cbe73816b816e50
Phu Cuong Nguyen <cuongnguyen@itrvn.com>
2024-04-06 22:52:43 +0700

    Rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tc_english",
+  base: "/tc_english/",
   plugins: [react()],
 });
